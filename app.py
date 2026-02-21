cefa6c20bfb53d846e3257e063bde1117bf9e2df
karthikaikannan-max <karthikaikannan@eee.sastra.edu>
2026-02-21 18:51:27 +0530

    fix render error

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from flask import Flask, render_template, request, send_file
 import subprocess
 
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+import tempfile
+import os
 
 @app.route('/generate', methods=['POST'])
 def generate():
@@ -19,12 +17,18 @@ def generate():
 \end{document}
 """
 
-    with open("output.tex","w",encoding="utf-8") as f:
-        f.write(tex)
+    with tempfile.TemporaryDirectory() as tempdir:
+        tex_path = os.path.join(tempdir, "output.tex")
 
-    subprocess.run(["pdflatex","output.tex"])
+        with open(tex_path, "w", encoding="utf-8") as f:
+            f.write(tex)
 
-    return send_file("output.pdf", as_attachment=True)
+        subprocess.run(
+            ["pdflatex", "-interaction=nonstopmode", "output.tex"],
+            cwd=tempdir
+        )
 
+        pdf_path = os.path.join(tempdir, "output.pdf")
+        return send_file(pdf_path, as_attachment=True)
 if __name__ == "__main__":
     app.run()
